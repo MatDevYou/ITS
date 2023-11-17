ed55d6ab575a088faa0e0e8dc8f097a220183cf5
Matteo Meringolo <149404046+MatDevYou@users.noreply.github.com>
2023-11-17 12:15:18 +0100

    Delete ITS2024 directory

--- ITS2024/Lezione1/cicloFor.js
+++
@@ -1,8 +0,0 @@
-//Ciclo For
-
-var volte = 10; 
-
-for (var i = 0; i < volte; i++){
-    console.log("tiro a canestro " + i)
-}
-
--- ITS2024/Lezione2/script4.js
+++
@@ -1,5 +0,0 @@
-const BTN = document.createElement('button')
-
-BTN.textContent = "Cliccami"
-
-document.body.appendChild(BTN) //aggiungere come figlio elemento di btn e di aggiungerlo al body 
