182913e8751f8f8acccc2e1313a10f25d56327c0
Matteo Meringolo <149404046+MatDevYou@users.noreply.github.com>
2023-12-04 14:02:14 +0100

    Add files via upload

---
+++ ITS2024/Lezione 5/Dadi/dado.js
@@ -0,0 +1,14 @@
+class Dado  {
+
+    constructor(nFacce, nColore){
+        this.facce = nFacce;
+        this.colore = nColore;
+    }
+
+    lancia(){
+        return Math.ceil(Math.random() * this.facce);
+    }
+
+
+
+}
